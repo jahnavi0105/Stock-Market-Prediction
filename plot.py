5054cd57b56500631369a60a5210b463e97f0747
Jahnavi Chennamsetty <63448333+jahnavi0105@users.noreply.github.com>
2022-01-23 19:52:21 +0530

    Create plot.py

---
+++ plot.py
@@ -0,0 +1,8 @@
+#plot
+
+plt.plot(actual_stock_price, color='red', label= 'Actual Google Stock Price')
+plt.plot(predicted_stock_price, color = 'blue', label = 'Predicted Google Stock Price')
+plt.title('Google Stock Price Prediction')
+plt.xlabel('Time')
+plt.ylabel('Google Stock Price')
+plt.legend()
